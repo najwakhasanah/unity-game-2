9043bcee2d35cda8fde015572276de14df9d143e
najwakhasanah <salsabillanajwakhasanah@gmail.com>
2024-08-10 20:24:03 +0700

    enemy

---
+++ unity project/Assets/script/Enemy/FSM/FSMTransition.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class FSMTransition 
+{
+   public FSMDecision Decision; //PlayerInRangeOfAttack -> True or False
+   public string TrueState; //CurrentState -> AttackState
+   public string FalseState; //CurrentState -> PatrolState
+
+}
--- unity project/Assets/script/Player/PlayerHealth.cs
+++ unity project/Assets/script/Player/PlayerHealth.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     [Header("Config")]
     [SerializeField] private PlayerStats stats;
-
+    
     private PlayerAnimations playerAnimations;
 
     private void Awake()
@@ -17,21 +14,24 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            TakeDamage(1f);
-        }
-    }
-
-    public void TakeDamage(float amount)
-    {
-        stats.Health -= amount;
         if (stats.Health <= 0f)
         {
             PlayerDead();
         }
     }
 
+    public void TakeDamage(float amount)
+    {
+        if (stats.Health <= 0f) return;
+        stats.Health -= amount;
+        DamageManager.Instance.ShowDamageText(amount, transform);
+        if (stats.Health <= 0f)
+        {
+            stats.Health = 0f;
+            PlayerDead();
+        }
+    }
+    
     private void PlayerDead()
     {
         playerAnimations.SetDeadAnimation();
