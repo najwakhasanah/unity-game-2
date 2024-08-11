e3da5bd594397d0d24fd04438ffad2476a0aa28e
najwakhasanah <salsabillanajwakhasanah@gmail.com>
2024-08-11 21:03:07 +0700

    attack 1

--- unity project/Assets/script/Player/Player.cs
+++ unity project/Assets/script/Player/Player.cs
@@ -7,12 +7,15 @@ public class Player : MonoBehaviour
     [SerializeField] private PlayerStats stats;
 
     public PlayerStats Stats => stats;
+    public PlayerMana PlayerMana {get; set;}
 
     private PlayerAnimations animations;
 
     private void Awake()
     {
+        PlayerMana = GetComponent<PlayerMana>();
         animations = GetComponent<PlayerAnimations>();
+        PlayerMana.ResetMana();
     }
 
     public void ResetPlayer()
--- unity project/Assets/script/Player/PlayerAttack.cs
+++ unity project/Assets/script/Player/PlayerAttack.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
-{
+{   
+    [Header("Config")]
+    [SerializeField] private Weapon initialWeapon;
+    [SerializeField] private Transform[] attackPositions;
+
     private PlayerActions actions;
     private PlayerAnimations playerAnimations;
+
+    private PlayerMovement playerMovement;
+
+    private PlayerMana playerMana;
     private EnemyBrain enemyTarget;
     private Coroutine attackCoroutine;
+
+    private Transform currentAttackPosition;
+    private float currentAttackRotation;
 
     private void Awake()
     {
         actions = new PlayerActions();
+        playerMana = GetComponent<PlayerMana>();
+        playerMovement =GetComponent<PlayerMovement>();
         playerAnimations = GetComponent<PlayerAnimations>();
     } 
 
@@ -20,6 +33,10 @@ public class PlayerAttack : MonoBehaviour
         actions.Attack.ClickAttack.performed += ctx => Attack();
     }
 
+    private void Update()
+    {
+        GetFirePosition();
+    }
     private void Attack()
     {
         if (enemyTarget == null) return;
@@ -31,12 +48,53 @@ public class PlayerAttack : MonoBehaviour
     }
 
     private IEnumerator IEAttack()
-    {
+    {   
+        if (currentAttackPosition != null)
+        {   
+            if (playerMana.CurrentMana < initialWeapon.RequiredMana) yield break;
+            Quaternion rotation = 
+                Quaternion.Euler(new Vector3(0f, 0f, currentAttackRotation));
+            Projectile projectile = Instantiate(initialWeapon.ProjectilePrefab, 
+                currentAttackPosition.position, rotation);
+            projectile.Direction = Vector3.up;
+            projectile.Damage = initialWeapon.Damage;;
+            playerMana.UseMana(initialWeapon.RequiredMana);
+        }
+
         playerAnimations.SetAttackAnimation(true);
         yield return new WaitForSeconds(0.5f);
         playerAnimations.SetAttackAnimation(false);
     }
 
+
+    private void GetFirePosition()
+    {
+        Vector2 moveDirection = playerMovement.MoveDirection;
+        switch (moveDirection.x)
+        {
+            case > 0f:
+                currentAttackPosition = attackPositions[1];
+                currentAttackRotation = -90f;
+                break; 
+            case < 0f:
+                currentAttackPosition = attackPositions[3];
+                currentAttackRotation = -270f;
+                break;
+        }
+        switch (moveDirection.y)
+        {
+            case > 0f:
+                currentAttackPosition = attackPositions[0];
+                currentAttackRotation = -90f;
+                break; 
+            case < 0f:
+                currentAttackPosition = attackPositions[2];
+                currentAttackRotation = -270f;
+                break;
+            
+        }
+
+    }
     private void EnemySelectedCallback(EnemyBrain enemySelected)
     {
         enemyTarget = enemySelected;
